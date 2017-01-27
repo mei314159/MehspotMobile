e7fb3940c3acfd3c673cdf8a5bc63d47b2ec3c5c
alex.v.minski <alex.v.minski@gmail.com>
2017-01-27 18:05:08 +0300

    switch to azure-api, publish ios app to hockeyapp

--- Mehspot.Core/Constants.cs
+++ Mehspot.Core/Constants.cs
@@ -3,7 +3,7 @@ namespace Mehspot.Core
 {
     public class Constants
     {
-        //public const string ApiHost = "http://api.mehspot.com";
-        public const string ApiHost = "http://192.168.1.3:8080";
+        public const string ApiHost = "http://mehspot-api-dev.azurewebsites.net";
+        //public const string ApiHost = "http://192.168.1.3:8080";
     }
 }
