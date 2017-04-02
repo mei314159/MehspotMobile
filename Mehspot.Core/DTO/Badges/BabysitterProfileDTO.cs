ad544b8eddd8f796273ce1df262ab5acfc372fc4
alex.v.minski <alex.v.minski@gmail.com>
2017-04-02 22:12:00 +0300

    fixed: profile not loading when hourlyRate is null

--- Mehspot.Core/DTO/Badges/BabysitterProfileDTO.cs
+++ Mehspot.Core/DTO/Badges/BabysitterProfileDTO.cs
@@ -6,7 +6,7 @@ namespace Mehspot.Core.DTO.Badges
     public class BabysitterProfileDTO:IBadgeProfileValues
     {
         public string FirstName { get; set; }
-        public double HourlyRate { get; set; }
+        public double? HourlyRate { get; set; }
         public string AgeRange { get; set; }
         public bool OwnCar { get; set; }
         public bool CanDrive { get; set; }
