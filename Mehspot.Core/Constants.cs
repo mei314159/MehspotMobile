b1dc3bd5c0064f73e424a90d30cbf62f5bcfe28c
alex.v.minski <alex.v.minski@gmail.com>
2017-04-19 18:00:56 +0300

    update provisioning profiles

--- Mehspot.Core/Constants.cs
+++ Mehspot.Core/Constants.cs
@@ -3,8 +3,8 @@ namespace Mehspot.Core
 {
     public class Constants
     {
-        //public const string ApiHost = "http://192.168.1.3";
-        public const string ApiHost = "http://mehspot-dev.mehspot.com";
+        //public const string ApiHost = "http://mehspot-dev.mehspot.com";
+        public const string ApiHost = "http://mehspot.com";
         //public const string ApiHost = "http://192.168.0.105";
     }
 }
