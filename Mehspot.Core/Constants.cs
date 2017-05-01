89bd7afe2e2f924997bbc6b769fad72da48df451
alex.v.minski <alex.v.minski@gmail.com>
2017-05-01 14:56:40 +0300

    fix api url

--- Mehspot.Core/Constants.cs
+++ Mehspot.Core/Constants.cs
@@ -4,8 +4,8 @@ namespace Mehspot.Core
     public class Constants
     {
         //public const string ApiHost = "http://mehspot-dev.mehspot.com";
-        //public const string ApiHost = "http://mehspot.com";
+        public const string ApiHost = "http://mehspot.com";
         //public const string ApiHost = "http://10.211.55.3";
-        public const string ApiHost = "http://192.168.0.105";
+        //public const string ApiHost = "http://192.168.0.105";
     }
 }
