bc09b98d807ae4630222971d8b2e0a8558c14d62
alex.v.minski <alex.v.minski@gmail.com>
2017-05-19 12:36:55 +0300

    fix default value for slider cell

--- mehspot.iOS/Views/Cell/SliderCell.cs
+++ mehspot.iOS/Views/Cell/SliderCell.cs
@@ -1,62 +1,71 @@
 ﻿using System;
-using System.Linq.Expressions;
 using Foundation;
-using mehspot.iOS.Extensions;
 using UIKit;
 
 namespace mehspot.iOS.Views.Cell
 {
-    public partial class SliderCell : UITableViewCell
-    {
-        public static readonly NSString Key = new NSString ("SliderCell");
-        public static readonly UINib Nib;
+	public partial class SliderCell : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString("SliderCell");
+		public static readonly UINib Nib;
 
-        static SliderCell ()
-        {
-            Nib = UINib.FromName ("SliderCell", NSBundle.MainBundle);
-        }
+		static SliderCell()
+		{
+			Nib = UINib.FromName("SliderCell", NSBundle.MainBundle);
+		}
 
-        protected SliderCell (IntPtr handle) : base (handle)
-        {
-            // Note: this .ctor should not contain any initialization logic.
-        }
+		protected SliderCell(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
 
-        public static SliderCell Create<TProperty> (int? defaultValue, Action<TProperty> setProperty, string placeholder, float? minValue, float? maxValue, bool isReadOnly = false)
-        {
-            var cell = (SliderCell)Nib.Instantiate (null, null) [0];
-            cell.CellSlider.Enabled = !isReadOnly;
-            cell.FieldLabel.Text = placeholder;
-            if (minValue.HasValue) {
-                cell.CellSlider.MinValue = minValue.Value - 1;
-            }
+		public static SliderCell Create<TProperty>(int? defaultValue, Action<TProperty> setProperty, string placeholder, float? minValue, float? maxValue, bool isReadOnly = false)
+		{
+			var cell = (SliderCell)Nib.Instantiate(null, null)[0];
+			cell.CellSlider.Enabled = !isReadOnly;
+			cell.FieldLabel.Text = placeholder;
+			if (minValue.HasValue)
+			{
+				cell.CellSlider.MinValue = minValue.Value - 1;
+			}
 
-            if (maxValue.HasValue) {
-                cell.CellSlider.MaxValue = maxValue.Value - 1;
-            }
+			if (maxValue.HasValue)
+			{
+				cell.CellSlider.MaxValue = maxValue.Value - 1;
+			}
 
-            cell.CellSlider.Value = defaultValue ?? cell.CellSlider.MinValue;
-            cell.SetValueLabel (cell.CellSlider.Value == cell.CellSlider.MinValue ? string.Empty : cell.CellSlider.Value.ToString ());
+			cell.CellSlider.Value = defaultValue ?? cell.CellSlider.MinValue;
+			cell.SetValueLabel(cell.CellSlider.Value == cell.CellSlider.MinValue ? string.Empty : cell.CellSlider.Value.ToString());
 
-            var propertyType = Nullable.GetUnderlyingType (typeof (TProperty)) ?? typeof (TProperty);
 
-            cell.CellSlider.ValueChanged += (sender, e) => {
-                var slider = (UISlider)sender;
-                var value = slider.Value;
-                var val = (TProperty)Convert.ChangeType (value, propertyType);
-                var clearValue = slider.Value == slider.MinValue;
-                if (clearValue) {
-                    val = default(TProperty);
-                }
+			cell.CellSlider.ValueChanged += (sender, e) =>
+			{
+				SetValue(setProperty, cell);
+			};
 
-                cell.SetValueLabel (clearValue ? string.Empty : val?.ToString ());
-                setProperty (val);
-            };
+			SetValue(setProperty, cell);
+			return cell;
+		}
 
-            return cell;
-        }
+		static void SetValue<TProperty>(Action<TProperty> setProperty, SliderCell cell)
+		{
+			var slider = cell.CellSlider;
+			var value = slider.Value;
+			var propertyType = Nullable.GetUnderlyingType(typeof(TProperty)) ?? typeof(TProperty);
+			var val = (TProperty)Convert.ChangeType(value, propertyType);
+			var clearValue = slider.Value == slider.MinValue;
+			if (clearValue)
+			{
+				val = default(TProperty);
+			}
 
-        private void SetValueLabel (string value) {
-            this.ValueLabel.Text = value;
-        }
-    }
+			cell.SetValueLabel(clearValue ? string.Empty : val?.ToString());
+			setProperty(val);
+		}
+
+		private void SetValueLabel(string value)
+		{
+			this.ValueLabel.Text = value;
+		}
+	}
 }
