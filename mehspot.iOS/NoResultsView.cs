55763ce6ad82e99b14d49724b451e09b35302d6b
alex.v.minski <alex.v.minski@gmail.com>
2017-08-17 12:20:30 +0300

    Delete register button for search with no results

--- mehspot.iOS/NoResultsView.cs
+++ mehspot.iOS/NoResultsView.cs
@@ -18,33 +18,10 @@ namespace mehspot.iOS
 		{
 		}
 
-		public event Action OnRegisterButtonTouched;
-
-		public static NoResultsView Create(string description)
+		public static NoResultsView Create()
 		{
 			var cell = (NoResultsView)Nib.Instantiate(null, null)[0];
-			cell.RegisterButton.Layer.BorderWidth = 1;
-			cell.RegisterButton.Layer.BorderColor = cell.RegisterButton.TitleColor(UIControlState.Normal).CGColor;
-			cell.Message.Text = description;
 			return cell;
 		}
-
-		partial void RegisterButtonTouched(UIButton sender)
-		{
-			this.OnRegisterButtonTouched?.Invoke();
-		}
-
-		public void HideRegisterButton()
-		{
-			RegisterButton.Hidden = true;
-			Message.Hidden = true;
-		}
-
-		public void ShowRegisterButton()
-		{
-			RegisterButton.Hidden = false;
-			Message.Hidden = false;
-		}
-
 	}
 }
