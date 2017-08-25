2839e6582392b8e8f620e2d72333101e5260939a
alex.v.minski <alex.v.minski@gmail.com>
2017-08-25 17:47:46 +0300

    iOS. Change labels, and filters

--- Mehspot.Core/DTO/Badges/BaseFilterDTO.cs
+++ Mehspot.Core/DTO/Badges/BaseFilterDTO.cs
@@ -1,9 +1,20 @@
 namespace Mehspot.Core.DTO.Search
 {
-    public class BaseFilterDTO
+    public interface IBaseFilterDTO
+    {
+        int? DistanceFrom { get; set; }
+
+        string ZipCode { get; set; }
+
+        bool? HasPicture { get; set; }
+
+        bool? HasReferences { get; set; }
+    }
+
+    public class BaseFilterDTO: IBaseFilterDTO
     {
         [Cell(Label = "Max Distance", CellType = CellType.Range, MinValue = 0, MaxValue = 200, DefaultValue = 20, Order = -2)]
-        public int? DistanceFrom { get; set; }
+        public virtual int? DistanceFrom { get; set; }
 
         public string ZipCode { get; set; }
 
@@ -14,4 +25,18 @@ namespace Mehspot.Core.DTO.Search
         public bool? HasReferences { get; set; }
     }
 
+
+    public class BabysitterFilterDTO : IBaseFilterDTO
+    {
+        [Cell(Label = "Max Distance", CellType = CellType.Range, MinValue = 0, MaxValue = 25, DefaultValue = 20, Order = -2)]
+        public int? DistanceFrom { get; set; }
+
+		public string ZipCode { get; set; }
+
+		[Cell(Label = "Has Profile Picture", CellType = CellType.Boolean, Order = 999)]
+		public bool? HasPicture { get; set; }
+
+		[Cell(Label = "Has References", CellType = CellType.Boolean, Order = 1000)]
+		public bool? HasReferences { get; set; }
+    }
 }
--- Mehspot.Core/DTO/Badges/SearchFilter/ISearchQueryDTO.cs
+++ Mehspot.Core/DTO/Badges/SearchFilter/ISearchQueryDTO.cs
@@ -3,14 +3,7 @@ namespace Mehspot.Core.DTO.Search
     public interface ISearchQueryDTO
     {
         int BadgeId { get; set; }
-        BaseFilterDTO Details { get; set; }
-    }
 
-    public class SearchFilterDTOBase : ISearchQueryDTO
-    {
-        public int BadgeId { get; set; }
-
-        [Cell(CellType = CellType.Complex, Order = 0)]
-        public BaseFilterDTO Details { get; set; } = new BaseFilterDTO();
+        IBaseFilterDTO Details { get; }
     }
 }
